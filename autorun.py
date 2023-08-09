66639f0c03de1be9758c91b73561740f474b8ca0
unknown <nkhbes001@myuct.ac.za>
2023-08-09 09:14:43 +0200

    I think I'm done

--- autorun.py
+++ autorun.py
@@ -1,10 +1,10 @@
 import os
 import statistics
-os.system("make")
+#os.system("make")
 # os.system("java -cp ./bin MonteCarloMini.MonteCarloMinimizationParallel 10000 10000 30 20 10 20 0.3")
 times = []
 for i in range(0, 5):
-    os.system("java -Xmx6g -cp ./bin MonteCarloMini.MonteCarloMinimizationParallel 10 10 -1000 1000 -1000 1000 0.1 > out.txt")
+    os.system("java -Xmx10g -cp ./bin MonteCarloMini.MonteCarloMinimizationParallel 500000 500000 -1000 1000 -1000 1000 1 > out.txt")
     with open("out.txt") as f:
         fl = f.read()
         time = eval(fl[fl.find("Time")+6:fl.find("ms")])
