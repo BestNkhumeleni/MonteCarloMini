5fef484cd87a8fe818687161a80f8d4538dfb2b7
unknown <nkhbes001@myuct.ac.za>
2023-08-10 14:31:24 +0200

    Fixed read me

--- autorun.py
+++ autorun.py
@@ -4,7 +4,7 @@ import statistics
 # os.system("java -cp ./bin MonteCarloMini.MonteCarloMinimizationParallel 10000 10000 30 20 10 20 0.3")
 times = []
 for i in range(0, 5):
-    os.system("java -Xmx10g -cp ./bin MonteCarloMini.MonteCarloMinimizationParallel 500000 500000 -1000 1000 -1000 1000 1 > out.txt")
+    os.system("java -Xmx10g -cp ./bin MonteCarloMini.MonteCarloMinimization 10000 10000 0 10 0 10 1")
     with open("out.txt") as f:
         fl = f.read()
         time = eval(fl[fl.find("Time")+6:fl.find("ms")])
